7ab330624c9e047e923088bad9193e28536d0558
Junnyjun <chbe5082@naver.com>
2022-07-04 23:32:34 +0900

    ✨ Feat(User) : entity 설정 변경

---
+++ src/main/kotlin/spring/action/kotlin/db/user/User.kt
@@ -0,0 +1,46 @@
+package spring.action.kotlin.db.user
+
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+import javax.persistence.*
+
+@Entity
+class User(
+    val userName: String,
+    val passWord:String,
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    var id: String? = null
+): UserDetails{
+
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        return mutableListOf(SimpleGrantedAuthority("ROLE_USER"))
+    }
+
+    override fun getPassword(): String? {
+        return password
+    }
+
+
+    override fun getUsername(): String {
+        return username
+    }
+
+    override fun isAccountNonExpired(): Boolean {
+        return true;
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true;
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true;
+    }
+
+    override fun isEnabled(): Boolean {
+        return true;
+    }
+
+}
